d927372987f60f9693de1863ce5a239949a3c9db
OmarZakariia <omar.zakaria88@hotmail.com>
2021-12-21 13:38:20 +0200

    Update  TravelMapViewController+MapViewMethods.swift
    Pins are now more pin like

--- Virtual Tourist 3.0/Controllers/TravelMapViewController+MapViewMethods.swift
+++ Virtual Tourist 3.0/Controllers/TravelMapViewController+MapViewMethods.swift
@@ -12,6 +12,25 @@ import CoreData
 extension TravelMapViewController: MKMapViewDelegate{
     
     
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+
+            let reuseId = "pin"
+
+            var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+
+            if pinView == nil {
+                pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+                pinView!.pinTintColor = .red
+            }
+            else {
+                pinView!.annotation = annotation
+            }
+
+
+            return pinView
+        }
+    
+    
     func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
         
         print("didSelct HAS BEEN CALLED")
